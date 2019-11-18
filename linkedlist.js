33213958c036e9c77f8d923dde7aa1c86177eb7a
edjunma <edjunma@gmail.com>
2019-11-18 04:27:33 -0500

    finish linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -2,14 +2,14 @@
 
 class LinkedList {
   constructor(value) {
-    this.head = {value, next: null};
+    this.head = { value, next: null };
     this.tail = this.head;
   }
 
   // Inserting value to the end of linked list
   insert(value) {
     // Create the nodes
-    const node = {value, next: null};
+    const node = { value, next: null };
     // Set the next property of the tail = to the new node
     this.tail.next = node;
     // The node is now the tail
@@ -20,6 +20,47 @@ class LinkedList {
   removeNode(val) {
     // Starting at the head of the linked list
     let currentNode = this.head;
+    // Hold reference to the previous node
+    let previousNode;
+    // While we have a node, passed the tail
+    while (currentNode) {
+      // If we find our value then break out
+      if (currentNode.value === val) {
+        break;
+      }
+      // No value found if currentNode is set to previousNode
+      previousNode = currentNodel;
+      // Then get the next node and assign it to the currentNode
+      currentNode = currentNode.next;
+    }
+    // Then return undefined as we have no node with that value
+    if (currentNode === null) {
+      return false;
+    }
 
+    // If the node is the head then we set the head to the next value of the head node
+    if (currentNode === this.head) {
+      this.head = this.head.next;
+      return;
+    }
+    // So then we remove our node by setting the node before it to the node in front of it
+    previousNode.next = currentNode.next;
+  }
+
+  // Remove tail method
+  removeTail() {
+    let currentNode = this.head;
+    let previousNode;
+    while (currentNode) {
+      if (!currentNode.next) {
+        break;
+      }
+      // reference to previous node
+      previousNode = currentNode;
+      // move onto the next node
+      currentNode = currentNode.next;
+    }
+    // to remove the tail, we set the previous node to null
+    previousNode.next = null;
   }
 }
