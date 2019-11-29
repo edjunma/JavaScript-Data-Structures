f2abd6d070eef0fdfdf2b67bd2f998506fe9aff8
edjunma <edjunma@gmail.com>
2019-11-29 01:34:47 -0500

    added 5 coding interview javascript practice questions

--- coding-interview.js
+++ coding-interview.js
@@ -37,3 +37,60 @@ function dequeue(stackInput, stackOutput) {
 
 	return stackOutput.pop();
 }
+
+// Q3: Write a "mul" function which will be properly invoked as below syntax
+console.log(mul(2)(3)(4)); // output: 24
+console.log(mul(4)(3)(4)); // output: 48
+
+function mul(x) {
+	return function(y) {
+		return function(z) {
+			return x * y * z;
+		};
+	};
+}
+
+// Q4: How to empty an array in JavaScript?
+// Multiple methods depending on situation
+var arrayList = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+// Method 1
+var anotherArrayList = arrayList; //Reference arrayList to another variable
+arrayList = []; //empty the array
+// Set the variable arrayList to a new empty array, recommended if you don't have references to the original arrayList anywhere else.
+
+// Method 2
+arrayList.length = 0;
+// This way clears the existing array by setting the length to 0.
+
+// Method 3
+arrayList.splice(0, arrayList.length);
+// This will work too because it will empty the length of the array
+
+// Method 4
+while (arrayList.length) {
+	arrayList.pop();
+}
+// Above method can empty array but not recommended
+
+// Q5: How to check if an object is an array or not?
+// The best way to find whether an object is an instance of a particular class or not using toString from Object.prototype
+var arrayList = [1, 2, 3];
+
+function greet(param) {
+	if (Object.prototype.toString.call(arrayList) === '[object Array]') {
+		console.log('Array!');
+	} else {
+		console.log('Not an array!');
+	}
+}
+
+// jQuery way
+if ($.isArray(arrayList)) {
+	console.log('Array!');
+} else {
+	console.log('Not an array!');
+}
+
+// Modern way to check now
+Array.isArray(arrayList);
